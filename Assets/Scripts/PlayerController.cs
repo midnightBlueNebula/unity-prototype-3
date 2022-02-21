c1b5302869f56e1ad1bf88b4600f363c9a783708
Onur Kaya <onurkaya8@outlook.com>
2022-02-21 14:00:45 +0300

    init project.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 600.0f;
+    private bool onGround = true;
+    public bool gameOver = false;
+    private Animator playerAnim;
+    public ParticleSystem explosion;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            dirtParticle.Stop();
+            onGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+
+        transform.position = new Vector3(0, transform.position.y, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+            dirtParticle.Play();
+        } else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            explosion.Play();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+            dirtParticle.Stop();
+        }
+    }
+}
